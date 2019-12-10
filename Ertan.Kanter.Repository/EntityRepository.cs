7e8ced28f3efdc5aa5c08abff72f855832bb25e4
Ertan Kanter <ertankanter@outlook.com>
2019-12-10 15:28:25 +0300

    manager katmanı eklendi.

--- Ertan.Kanter.Repository/EntityRepository.cs
+++ Ertan.Kanter.Repository/EntityRepository.cs
@@ -4,15 +4,12 @@ using System.Text;
 
 namespace Ertan.Kanter.Repository
 {
-    public class EntityRepository
+    public interface IDataRepository<TEntity>
     {
-        public interface IDataRepository<TEntity>
-        {
-            IEnumerable<TEntity> GetAll();
-            TEntity Get(long id);
-            void Add(TEntity entity);
-            void Update(TEntity dbEntity, TEntity entity);
-            void Delete(TEntity entity);
-        }
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+        void Add(TEntity entity);
+        void Update(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
     }
 }
---
+++ ErtanKanter.Manager/ArticleManager.cs
@@ -0,0 +1,53 @@
+﻿using Ertan.Kanter.Repository;
+using ErtanKanter.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErtanKanter.Manager
+{
+    public class ArticleManager : IDataRepository<Article>
+    {
+        readonly ArticleContext _ArticleContext;
+
+        public ArticleManager(ArticleContext context)
+        {
+            _ArticleContext = context;
+        }
+
+        public IEnumerable<Article> GetAll()
+        {
+            return _ArticleContext.Articles.ToList();
+        }
+
+        public Article Get(long id)
+        {
+            return _ArticleContext.Articles.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Add(Article entity)
+        {
+            _ArticleContext.Articles.Add(entity);
+            _ArticleContext.SaveChanges();
+        }
+
+        public void Update(Article employee, Article entity)
+        {
+            employee.ImagePath = entity.ImagePath;
+            employee.Subject = entity.Subject;
+            employee.Content = entity.Content;
+            employee.Date = entity.Date;
+            employee.Author = entity.Author;
+            employee.IsActive = entity.IsActive;
+
+            _ArticleContext.SaveChanges();
+        }
+
+        public void Delete(Article employee)
+        {
+            _ArticleContext.Articles.Remove(employee);
+            _ArticleContext.SaveChanges();
+        }
+    }
+}
