e69b73704553f361bb8099ed407a29d5b9f62907
Ertan Kanter <ertankanter@outlook.com>
2019-12-10 15:19:57 +0300

    repository katmanı eklendi

---
+++ Ertan.Kanter.Repository/EntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ertan.Kanter.Repository
+{
+    public class EntityRepository
+    {
+        public interface IDataRepository<TEntity>
+        {
+            IEnumerable<TEntity> GetAll();
+            TEntity Get(long id);
+            void Add(TEntity entity);
+            void Update(TEntity dbEntity, TEntity entity);
+            void Delete(TEntity entity);
+        }
+    }
+}
